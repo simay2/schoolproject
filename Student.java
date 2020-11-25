205960959c40d66131775333e511f43dc01ca782
simay2 <72138608+simay2@users.noreply.github.com>
2020-11-25 09:16:41 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+package edu.schooll;
+
+public class Student {
+	public String name;
+	public double grade;
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Student ( String name, double grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+
+}
--- TVTest.java
+++ TVTest.java
@@ -6,11 +6,11 @@ public class TVTest {
 		// TODO Auto-generated method stub
 		
 		TV tv1 = new TV();
-		System.out.println("channel" + tv1.channel + "volume" + tv1.volume + "on: " + tv1.on );
+		System.out.println("channel" + tv1.channel + "volume" + tv1.volume + " on: " + tv1.on );
 		tv1.switch0n();
 		tv1.channelUp();
 		tv1.volumeDown();
-		System.out.println("channel" + tv1.channel + "volume" + tv1.volume + "on: " + tv1.on );
+		System.out.println("channel" + tv1.channel + "volume" + tv1.volume + " on: " + tv1.on );
 
 	}
 
