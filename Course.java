96b97849048b1451e74838cb0d7951abfc716215
simay2 <72138608+simay2@users.noreply.github.com>
2020-12-11 09:32:27 +0200

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,44 @@
+package lessons.ten.fourteen;
+
+public class Course {
+	private String courseName;
+	private String[] students = new String[100];
+	private int number0fStudents = 0;
+	
+	public Course(String courseName) {
+		this.courseName = courseName;
+	}
+	
+	public void addStudent(String student) {
+		students[number0fStudents] = student;
+		number0fStudents++;
+	}
+	
+	public void dropStudet(String student) {
+		for(int i = 0; i < number0fStudents; i++) {
+			if(students[i].equals(student)) {
+				for(int j = i; j < number0fStudents - 1; j++ ) {
+					students[j] = students[j + 1];
+				}
+				
+				students[number0fStudents - 1] = null;
+				number0fStudents--;
+				break;
+			}
+		}
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+    public String[] getStudents() {
+    	return students;
+	}
+    
+    public int getNumber0fStudents() {
+    	return number0fStudents;
+    }
+	
+
+}
---
+++ CourseTest.java
@@ -0,0 +1,31 @@
+package lessons.ten.fourteen;
+
+public class CourseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Course course1 = new Course("Data Structures");
+		Course course2 = new Course("Database Systems");
+		course1.addStudent("Peter Jones");
+		course1.addStudent("Kim Smith");
+		course1.addStudent("Anne Kennedy");
+		course2.addStudent("Peter Jones");
+		course2.addStudent("Steve Smith");
+		
+		System.out.println("Number of students in course1: " + course1.getNumber0fStudents());
+		String[] students = course1.getStudents();
+		for( int i = 0; i < course1.getNumber0fStudents(); i++) {
+			System.out.print(students[i] + " , ");
+		}
+		
+		course1.dropStudet("Kim Smith");
+		System.out.println("\nNumber of students in course1: " + course1.getNumber0fStudents());
+		for( int i = 0; i < course1.getNumber0fStudents(); i++) {
+			System.out.print(students[i] + " , ");
+		}
+		
+		System.out.println();
+		System.out.print("Number of students in course2: " + course2.getNumber0fStudents());
+	}
+
+}
